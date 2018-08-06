37518ef6fe65cf461d02d6b788a66f948e8bcebb
jentino <jentino@gmail.com>
2018-08-06 10:59:26 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+document.getElementById("app").innerHTML = `
+<h1>Hello Parcel!</h1>
+<div>
+  Look
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
+  for morasdfe info about Parcel.
+</div>
+`;
+
+document.getElementById("app2").innerHTML = `
+<h1>Hello Parcel 2!</h1>
+<div>
+  Look here too
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
+  for more info about Parcel.
+</div>
+`;
